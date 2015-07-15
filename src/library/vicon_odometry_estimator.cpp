ce194fc5c45f8be21dfe5e01ab91cc6c323c4e55
Alex Millane <millanea@ethz.ch>
2015-07-15 10:49:48 +0200

    Updated estimator to write intermediate values to new topic.

--- src/library/vicon_odometry_estimator.cpp
+++ src/library/vicon_odometry_estimator.cpp
@@ -163,26 +163,29 @@ void RotationalEstimator::updateEstimate(const Eigen::Quaterniond& quat_measured
 	updateEstimate_recombineErrorGlobal(&x_m, &dx_m, x_p, &msg);
 
 	// Extracting estimated quantities from the posteriori state
-/*	quat_hat = Eigen::Quaterniond(x_m.block<4,1>(0,0));
+	quat_hat = Eigen::Quaterniond(x_m.block<4,1>(0,0));
 	omega_hat = x_m.block<3,1>(4,0);
-	covariance = P_m;*/
-	quat_hat = Eigen::Quaterniond(x_old.block<4,1>(0,0));
+	covariance = P_m;
+/*	quat_hat = Eigen::Quaterniond(x_old.block<4,1>(0,0));
 	omega_hat = x_old.block<3,1>(4,0);
-	covariance = P_old;
+	covariance = P_old;*/
 
-	// Outputting the messages to the console
+/*	// Outputting the messages to the console
+	std::cout << "quat_measured: " << std::endl << quat_measured.coeffs() << std::endl ;
   std::cout << "quat_hat: " << std::endl << quat_hat.coeffs() << std::endl ;
 	std::cout << "omega_hat: " << std::endl << omega_hat << std::endl ;
-
+*/
 	// Publishing estimator message
   publisher.publish(msg) ;
 }
 
-Eigen::Quaterniond RotationalEstimator::getEstimatedOrientation() const {
+Eigen::Quaterniond RotationalEstimator::getEstimatedOrientation() const
+{
   return quat_hat ;
 }  
 
-Eigen::Vector3d RotationalEstimator::getEstimatedAngularVelocity() const {
+Eigen::Vector3d RotationalEstimator::getEstimatedAngularVelocity() const
+{
   return omega_hat ;
 }
 
@@ -227,12 +230,19 @@ void RotationalEstimator::updateEstimate_propagateErrorEstimate(Eigen::Matrix<do
 	Eigen::Matrix<double, 3, 1> dQuat_hat = dx_old.block<3,1>(0, 0) ;
 	Eigen::Matrix<double, 3, 1> dOmega_hat = dx_old.block<3,1>(3, 0) ;
 	// Performing propagation
-	Eigen::Vector3d dQuat_hat_p_ROC = -omega_hat.cross(dQuat_hat) + 0.5*dOmega_hat ;
+	Eigen::Vector3d dQuat_hat_p_ROC = -omega_hat.cross(dQuat_hat) + 0.5*dOmega_hat ; // Appears to agree with equations in papers
 	Eigen::Vector3d dOmega_hat_p_ROC = Eigen::Vector3d::Zero() ;
 	Eigen::Vector3d dQuat_hat_p = dQuat_hat + dQuat_hat_p_ROC*estimator_parameters_.dt ;
 	Eigen::Vector3d dOmega_hat_p = dOmega_hat + dOmega_hat_p_ROC*estimator_parameters_.dt ;
 	// Writing to apriori error state
 	*dx_p << dQuat_hat_p, dOmega_hat_p ;
+	// Writing to the message object
+  msg->dQuat_p.x = dQuat_hat_p.x() ;
+  msg->dQuat_p.y = dQuat_hat_p.y() ;
+  msg->dQuat_p.z = dQuat_hat_p.z() ;
+  msg->dOmega_p.x = dOmega_hat_p.x() ;
+  msg->dOmega_p.y = dOmega_hat_p.y() ;
+  msg->dOmega_p.z = dOmega_hat_p.z() ;
 }
 
 void RotationalEstimator::updateEstimate_propagateErrorCovariance(Eigen::Matrix<double, 6, 6>* P_p, Eigen::Matrix<double, 6, 6>& P_old, const Eigen::Matrix<double, 7, 1>& x_old,
@@ -244,10 +254,10 @@ void RotationalEstimator::updateEstimate_propagateErrorCovariance(Eigen::Matrix<
 	// Constructing linearized system matrices
   Eigen::Matrix<double, 6, 6> A;
 	Eigen::Matrix<double, 6, 6> L;
-	A << skewMatrix(omega_hat), 0.5*Eigen::Matrix3d::Identity(), Eigen::Matrix3d::Zero(), Eigen::Matrix3d::Zero() ;
+	A << skewMatrix(omega_hat), 0.5*Eigen::Matrix3d::Identity(), Eigen::Matrix3d::Zero(), Eigen::Matrix3d::Zero() ; // Should be accordance with the hamilton definition
 	L << Eigen::Matrix3d::Identity(), Eigen::Matrix3d::Zero(), Eigen::Matrix3d::Zero(), Eigen::Matrix3d::Identity() ;
 	// Performing propagation
-	*P_p = covariance + ( A*P_old + P_old*A + L*processCovariance*L.transpose() )*estimator_parameters_.dt ;
+	*P_p = P_old + ( A*P_old + P_old*A + L*processCovariance*L.transpose() )*estimator_parameters_.dt ;
 }
 
 void RotationalEstimator::updateEstimate_updateErrorEstimate(Eigen::Matrix<double, 6, 1>* dx_m, Eigen::Matrix<double, 6, 6>* P_m, const Eigen::Quaterniond& quat_measured,
@@ -274,6 +284,16 @@ void RotationalEstimator::updateEstimate_updateErrorEstimate(Eigen::Matrix<doubl
 	*dx_m = dx_p + K*(quat_measured.coeffs() - quat_predicted.coeffs()) ; // TODO: Everything is going to be the wrong way around here
 	// Updating the covariance
 	*P_m = (Eigen::Matrix<double, 6, 6>::Identity() - K*H)*P_p ;
+	// Seperating the state parts
+	Eigen::Matrix<double, 3, 1> dQuat_hat_m = dx_m->block<3,1>(0,0);
+	Eigen::Matrix<double, 3, 1> dOmega_hat_m = dx_m->block<3,1>(3,0);
+	// Writing to the message object
+  msg->dQuat_m.x = dQuat_hat_m.x() ;
+  msg->dQuat_m.y = dQuat_hat_m.y() ;
+  msg->dQuat_m.z = dQuat_hat_m.z() ;
+  msg->dOmega_m.x = dOmega_hat_m.x() ;
+  msg->dOmega_m.y = dOmega_hat_m.y() ;
+  msg->dOmega_m.z = dOmega_hat_m.z() ;
 }
 
 void RotationalEstimator::updateEstimate_recombineErrorGlobal(Eigen::Matrix<double, 7, 1>* x_m, Eigen::Matrix<double, 6, 1>* dx_m, const Eigen::Matrix<double, 7, 1> x_p,
@@ -294,6 +314,13 @@ void RotationalEstimator::updateEstimate_recombineErrorGlobal(Eigen::Matrix<doub
 	// Writing to posteriori global state and error states
 	*x_m << quat_hat_m.coeffs(), omega_hat_m ;
 	*dx_m = Eigen::Matrix<double, 6, 1>::Zero() ;
+	// Writing to the message object
+  msg->quat_m.x = quat_hat_m.x() ;
+  msg->quat_m.y = quat_hat_m.y() ;
+  msg->quat_m.z = quat_hat_m.z() ;
+  msg->omega_m.x = omega_hat_m.x() ;
+  msg->omega_m.y = omega_hat_m.y() ;
+  msg->omega_m.z = omega_hat_m.z() ;
 }
 
 
